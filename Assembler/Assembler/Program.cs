a50793db12373d7b89b0ada8c7894020fc44aa70
unknown <mark@.(none)>
2011-04-02 14:35:08 -0400

    Added lists of keywords for instructions/functions and directives.

--- Assembler/Assembler/Program.cs
+++ Assembler/Assembler/Program.cs
@@ -9,6 +9,7 @@ namespace Assembler
         static void Main(string[] args)
         {
 			Console.WriteLine("Herp derp");
+            Console.WriteLine(Properties.Resources.directives);
         }
     }
 }
