dd13f9bbb3cabf8d37efb0dc574a830445bfa813
unknown <mark@.(none)>
2011-04-08 15:48:50 -0400

    Finished documenting IntermediateLine. Added logging. Herp derp.

--- Assembler/Assembler/IntermediateLine.cs
+++ Assembler/Assembler/IntermediateLine.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace Assembler
 {
@@ -72,6 +73,9 @@ namespace Assembler
          */
         public IntermediateLine(string source, short lineNum)
         {
+            Trace.WriteLine(String.Format("{0} -> {1}",
+                                           System.DateTime.Now, 
+                                           "Initializing line " + lineNum), "IntermediateLine");
             this.source = source;
             this.line = lineNum.ToString();
             this.LC = null;
@@ -86,7 +90,7 @@ namespace Assembler
         }
 
         /**
-         * Allows access to the source line in this object.
+         * Allows access for getting the full source line in this object.
          */
         public string SourceLine
         {
@@ -94,7 +98,7 @@ namespace Assembler
         }
 
         /**
-         * Allows access to the line number of this source line.
+         * Allows access for getting the line number of this source line.
          */
         public string SourceLineNumber
         {
@@ -102,7 +106,8 @@ namespace Assembler
         }
 
         /**
-         * herp derp
+         * Allows access for getting and setting the program counter value
+         * associated with this source line.
          */
         public string ProgramCounter
         {
@@ -110,59 +115,109 @@ namespace Assembler
             set { this.LC = value; }
         }
 
+        /**
+         * Allows access for getting and setting the label assigned
+         * to this source line.
+         */
         public string Label
         {
             get { return this.lineLabel == null ? "N/A" : this.lineLabel; ; }
             set { this.lineLabel = value; }
         }
 
+        /**
+         * Allows access for getting and setting the category of the function
+         * in this line of source.
+         */
         public string OpCategory
         {
             get { return this.category == null ? "N/A" : this.category; }
             set { this.category = value; }
         }
 
+        /**
+         * Allows access for getting and setting the function name in this line
+         * of source.
+         */
         public string OpFunction
         {
             get { return this.function == null ? "N/A" : this.function; }
             set { this.function = value; }
         }
 
+        /**
+         * Allows access for getting and setting the operand of the function
+         * in this line of source.
+         */
         public string OpOperand
         {
             get { return this.operand == null ? "N/A" : this.operand; }
             set { this.operand = value; }
         }
 
+        /**
+         * Allows access for getting and setting the literal operand of the
+         * function in this line of source.
+         */
         public string OpLitOperand
         {
             get { return this.operandLit == null ? "N/A" : this.operandLit; }
             set { this.operandLit = value; }
         }
 
+        /**
+         * Allows access for getting and setting the directive name in this
+         * line of source.
+         */
         public string Directive
         {
             get { return this.directive == null ? "N/A" : this.directive; }
             set { this.directive = value; }
         }
 
+        /**
+         * Allows access for getting and setting the directive operand in this
+         * line of source.
+         */
         public string DirectiveOperand
         {
             get { return this.dirOperand == null ? "N/A" : this.dirOperand; }
             set { this.dirOperand = value; }
         }
 
+        /**
+         * Allows access for getting and setting the comment at the end of this
+         * line of source.
+         */
         public string Comment
         {
             get { return this.comment == null ? "N/A" : this.comment; }
             set { this.comment = value; }
         }
 
+        /**
+         * Displays the line of source and its individual parts. The format of this is:<br />
+         * [SourceLine]<br />
+         * Line: [SourceLineNumber]<br />
+         * LC: [ProgramCounter]<br />
+         * Label: [Label]<br />
+         * Category: [OpCategory]<br />
+         * Function: [OpFunction]<br />
+         * Operand: [OpOperand]<br />
+         * Literal Operand: [OpLitOperand]<br />
+         * Directive: [Directive]<br />
+         * Directive Operand: [DirectiveOperand]<br />
+         * Comment: [Comment]
+         */
         public override string ToString()
         {
+            Trace.WriteLine(String.Format("{0} -> {1}", 
+                                           System.DateTime.Now, 
+                                           "Printing line " + this.line), "IntermediateLine");
+
             string format = "{1}\nLine: {2}{0}LC: {3}{0}Label: {4}{0}Category: {5}{0}Function: " +
             "{6}{0}Operand: {7}{0}Literal Operand: {8}{0}Directive: {9}{0}Directive Operand:{10}{0}Comment: {11}";
-            string seperator = "\t";
+            string seperator = "\n";
             return String.Format(format, seperator, this.SourceLine, this.SourceLineNumber, this.ProgramCounter,
                 this.Label, this.OpCategory, this.OpFunction, this.OpOperand, this.OpLitOperand,
                 this.Directive, this.DirectiveOperand, this.Comment);
