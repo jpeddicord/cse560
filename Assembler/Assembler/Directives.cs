39030d27f15e0b517c01fd78d943725600b3bc49
Mark Mathis <mathis.62@osu.edu>
2011-04-09 14:23:24 -0400

    Documented Directives class.  Removed DirList property from Directives class. Updated testin.txt to reflect the new sample program emailed to us.

--- Assembler/Assembler/Directives.cs
+++ Assembler/Assembler/Directives.cs
@@ -6,19 +6,37 @@ using System.Diagnostics;
 
 namespace Assembler
 {
+    /**
+     * A singleton class that enables us to pass the list of directives around the program.
+     */
     class Directives
     {
+        /**
+         * The single instance of this class.
+         */
         private static Directives instance;
+
+        /**
+         * The list of directives.
+         */
         private static ArrayList directiveList;
 
+        /**
+         * A private constructor that fills the list with the directives.
+         */
         private Directives()
         {
             Trace.WriteLine(String.Format("{0} -> {1}", DateTime.Now, "Creating instance of Directives."), "Directives");
             directiveList = new ArrayList(Properties.Resources.directives.Split('\n'));
         }
 
+        /**
+         * This is called to retrieve or create the single instance of the Directives class.
+         */
         public static Directives GetInstance()
         {
+            Trace.WriteLine(String.Format("{0} -> {1}", DateTime.Now, 
+                "Request for instance of Directives."), "Directives");
             if (Directives.instance == null)
             {
                 instance = new Directives();
@@ -27,11 +45,9 @@ namespace Assembler
             return instance;
         }
 
-        public ArrayList DirList
-        {
-            get { return directiveList; }
-        }
-
+        /**
+         * Returns true if dir is contained in the list of directives. False, otherwise.
+         */
         public bool Contains(string dir)
         {
             return directiveList.Contains(dir.ToUpper());
