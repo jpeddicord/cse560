42d8ffa69372783ea4e8ebd6b6d545f07f97dd37
Jacob Peddicord <jacob@peddicord.net>
2011-04-09 17:04:17 -0400

    Generate Doxygen pages for our RST manuals.

--- Assembler/doc/generate-docs.sh
+++ Assembler/doc/generate-docs.sh
@@ -2,17 +2,21 @@
 
 cd `dirname $0`
 
+# generate hand-crafted documentation
+echo "Generating hand-made documentation..."
+cd src
+mkdir -p rst-out
+for F in *.rst; do
+    echo "    $F"
+    rst2html --template=../template/rst.html $F > rst-out/${F%.rst}.html
+done
+cd ..
+
 # generate api documentation
 echo "Running doxygen..."
 doxygen >/dev/null
 
-# generate hand-crafted documentation
-echo "Generating hand-made documentation..."
-mkdir -p api/html/rst
-cd src
-for F in *.rst; do
-    echo "    $F"
-    rst2html $F > ../out/${F%.rst}.html
-done
+# clean up
+rm -rf src/rst-out
 
 echo "Done."
