0c4694f41a0ccfad3f6ee0aa7750c627a22558b7
Andrew Buelow <Andrew@.(none)>
2011-04-09 16:23:24 -0400

    Added text fixures for Tokenizer and documented them in test_plan.rst.

--- Assembler/Assembler/Tests/InstructionsTest.cs
+++ Assembler/Assembler/Tests/InstructionsTest.cs
@@ -28,5 +28,218 @@ namespace Assembler
         }
 
     }
+
+    [TestFixture]
+    public class TokenizerTest
+    {
+        /**
+         * [T1] Simple test to ensure the tokenizer can correctly return a token.
+         */
+        [Test]
+        public void TokenizerSimple()
+        {
+            string Line = "MOPER ADD,2";
+            string Token = "";
+            Tokenizer.TokenKinds TokenKind = Tokenizer.TokenKinds.Empty;
+            Tokenizer.GetNextToken(ref Line, ref Token, ref TokenKind);
+            Assert.IsTrue(Token == "MOPER" && TokenKind == Tokenizer.TokenKinds.Label_Or_Command,
+                "Token is 'MOPER' and of kind label.");
+        }
+
+        /**
+         * [T2] Tests if the tokenizer will return a token when the line begins with lots of whitespace.
+         */
+        [Test]
+        public void TokenizerWhiteSpaceBeginning()
+        {
+            string Line = "            MOPER ADD,2";
+            string Token = "";
+            Tokenizer.TokenKinds TokenKind = Tokenizer.TokenKinds.Empty;
+            Tokenizer.GetNextToken(ref Line, ref Token, ref TokenKind);
+            Assert.IsTrue(Token == "MOPER" && TokenKind == Tokenizer.TokenKinds.Label_Or_Command,
+                "Line begins with lots of whitespace.");
+        }
+
+        /**
+         * [T3] Tokenize an entire line and ensure all tokens are returned.
+         */
+        [Test]
+        public void TokenizerEntireLine()
+        {
+            string Line = " MOPER ADD,2";
+            string Token = "";
+            Tokenizer.TokenKinds TokenKind = Tokenizer.TokenKinds.Empty;
+            string[] expected = { "MOPER", "ADD", "2" };
+            string[] actual = new string[3];
+
+            int i = 0;
+            while (Line.Length > 0)
+            {
+                Tokenizer.GetNextToken(ref Line, ref Token, ref TokenKind);
+                actual[i] = Token;
+                i++;
+            }
+
+            Assert.AreEqual(expected, actual, "Tokenizing an entire line.");
+        }
+
+        /**
+         * [T4] Tokenize an entire line with extra whitespace and ensure all tokens are returned.
+         */
+        [Test]
+        public void TokenizerEntireLineWS()
+        {
+            string Line = "     SOPER        ADD,X=55        :I'm a comment    ";
+            string Token = "";
+            Tokenizer.TokenKinds TokenKind = Tokenizer.TokenKinds.Empty;
+            string[] expected = { "SOPER", "ADD", "X=55", ":I'm a comment" };
+            string[] actual = new string[4];
+
+            int i = 0;
+            while (Line.Length > 0)
+            {
+                Tokenizer.GetNextToken(ref Line, ref Token, ref TokenKind);
+                actual[i] = Token;
+                i++;
+            }
+
+            Assert.AreEqual(expected, actual, "Tokenizing an entire line with extra whitespace.");
+        }
+
+        /**
+         * [T5] Tokenize an entire line with mixed case to ensure tokens are correct.
+         */
+        [Test]
+        public void TokenizerEntireLineMixedCaseToken()
+        {
+            string Line = " mOpER AdD,x=aBc4";
+            string Token = "";
+            Tokenizer.TokenKinds TokenKind = Tokenizer.TokenKinds.Empty;
+            string[] TokenExpected = { "mOpER", "AdD", "x=aBc4" };
+            string[] TokenActual = new string[3];
+
+            int i = 0;
+            while (Line.Length > 0)
+            {
+                Tokenizer.GetNextToken(ref Line, ref Token, ref TokenKind);
+                TokenActual[i] = Token;
+                i++;
+            }
+
+            Assert.AreEqual(TokenExpected, TokenActual,
+                            "Tokenize entire line with mized case.");
+        }
+
+        /**
+         * [T6] Tokenize an entire line with mixed case to ensure both the token kinds are correct.
+         */
+        [Test]
+        public void TokenizerEntireLineMixedCaseKind()
+        {
+            string Line = " mOpER AdD,x=aBc4";
+            string Token = "";
+            Tokenizer.TokenKinds TokenKind = Tokenizer.TokenKinds.Empty;
+            Tokenizer.TokenKinds[] KindExpected = { Tokenizer.TokenKinds.Label_Or_Command,
+                                                    Tokenizer.TokenKinds.Label_Or_Command,
+                                                    Tokenizer.TokenKinds.Literal };
+            Tokenizer.TokenKinds[] KindActual = new Tokenizer.TokenKinds[3];
+
+            int i = 0;
+            while (Line.Length > 0)
+            {
+                Tokenizer.GetNextToken(ref Line, ref Token, ref TokenKind);
+                KindActual[i] = TokenKind;
+                i++;
+            }
+
+            Assert.AreEqual(KindExpected, KindActual,
+                            "Tokenize entire line with mized case.");
+        }
+
+        /**
+         * [T7] Test that it can correctly tokenize a Label_Or_Command.
+         */
+        [Test]
+        public void TokenizerLOC()
+        {
+            string Line = " CNTL CLEAR";
+            string Token = "";
+            Tokenizer.TokenKinds TokenKind = Tokenizer.TokenKinds.Empty;
+            Tokenizer.GetNextToken(ref Line, ref Token, ref TokenKind);
+            Assert.IsTrue(Token == "CNTL" && TokenKind == Tokenizer.TokenKinds.Label_Or_Command,
+                "Token is 'CNTL' and of kind label.");
+        }
+
+        /**
+         * [T8] Test that it can correctly tokenize a Literal.
+         */
+        [Test]
+        public void TokenizerLiteral()
+        {
+            string Line = "B=101101  :Comment";
+            string Token = "";
+            Tokenizer.TokenKinds TokenKind = Tokenizer.TokenKinds.Empty;
+            Tokenizer.GetNextToken(ref Line, ref Token, ref TokenKind);
+            Assert.IsTrue(Token == "B=101101" && TokenKind == Tokenizer.TokenKinds.Literal,
+                "Token is 'B=101101' and of kind Literal.");
+        }
+
+        /**
+         * [T9] Test that it can correctly tokenize a Comment.
+         */
+        [Test]
+        public void TokenizerComment()
+        {
+            string Line = ": This is a comment with whitespace, and commas that shouldn't be removed.";
+            string Token = "";
+            Tokenizer.TokenKinds TokenKind = Tokenizer.TokenKinds.Empty;
+            Tokenizer.GetNextToken(ref Line, ref Token, ref TokenKind);
+            Assert.IsTrue(Token == ": This is a comment with whitespace, and commas that shouldn't be removed."
+                && TokenKind == Tokenizer.TokenKinds.Comment,
+                "Token is of kind Comment.");
+        }
+
+        /**
+        * [T10] Test that it can correctly tokenize a Number.
+        */
+        [Test]
+        public void TokenizerNumber()
+        {
+            string Line = "9283 X4";
+            string Token = "";
+            Tokenizer.TokenKinds TokenKind = Tokenizer.TokenKinds.Empty;
+            Tokenizer.GetNextToken(ref Line, ref Token, ref TokenKind);
+            Assert.IsTrue(Token == "9283" && TokenKind == Tokenizer.TokenKinds.Number,
+                "Token is '9283' and of kind Number.");
+        }
+
+        /**
+        * [T11] Test that it can correctly tokenize an Empty.
+        */
+        [Test]
+        public void TokenizerEmpty()
+        {
+            string Line = "";
+            string Token = "";
+            Tokenizer.TokenKinds TokenKind = Tokenizer.TokenKinds.Empty;
+            Tokenizer.GetNextToken(ref Line, ref Token, ref TokenKind);
+            Assert.IsTrue(Token == "" && TokenKind == Tokenizer.TokenKinds.Empty,
+                "Token is '' and of kind Empty.");
+        }
+
+        /**
+        * [T12] Test that it can correctly tokenize an Error.
+        */
+        [Test]
+        public void TokenizerError()
+        {
+            string Line = "Go_to MOPER ADD,5";
+            string Token = "";
+            Tokenizer.TokenKinds TokenKind = Tokenizer.TokenKinds.Empty;
+            Tokenizer.GetNextToken(ref Line, ref Token, ref TokenKind);
+            Assert.IsTrue(Token == "Go_to" && TokenKind == Tokenizer.TokenKinds.Error,
+                "Token is 'Go_to' and of kind Error.");
+        }
+    }
 }
 
--- Assembler/doc/test_plan.rst
+++ Assembler/doc/test_plan.rst
@@ -15,3 +15,68 @@ Unit Testing
 ============
 
 Unit testing will use the NUnit testing framework. Each component will have several tests associated to throughly test the functionality and correctnes of the result.
+
+
+
+Tokenizer Test List
+===================
+
+Test Number: T1
+Purpose: This test is just to ensure basic functionality of the Tokenizer is being met.  Given a line of code, the Tokenizer should return the first word (all characters before the first space, comma, tab, or end of the line).
+Expected Results Achieved: Yes
+Concerns: None
+
+Test Number: T2
+Purpose: This test is used to ensure the tokenizer is unaffected by adding extra whitespace.  It should remove all padding so that the token does not have any whitespace in it.
+Expected Results Achieved: Yes
+Concerns: None
+
+Test Number: T3
+Purpose: To tokenize an entire line and ensure that all tokens in the line are correctly identified and returned.
+Expected Results Achieved: Yes
+Concerns: None
+
+Test Number: T4
+Purpose: To tokenize an entire line with added whitespace and ensure all unneeded whitespace is properly removed and all tokens are identified and returned.
+Expected Results Achieved: Yes
+Concerns: If whitespace is added in the operand field, it will not recognize that it shouldn't be there.  This should be handled while parsing.
+
+Test Number: T5
+Purpose: Tokenizing a line of code that has mixed case letters.  The user shouldn't have to worry about the case they are writing the code in so the tokenizer should be able to handle both upper and lower case.
+Expected Results Achieved: Yes
+Concerns: Unable to properly check both token text and kind.  Will add another test.
+
+Test Number: T6
+Purpose: This test is an extension of test number T5.  Again it is testing a line written in mixed case, however this time it is checking the token kinds returned are correct instead of the token text.
+Expected Results Achieved: Yes
+Concerns: None
+
+Test Number: T7
+Purpose: Ensures the Tokenizer is able to correctly identify a token of kind Label_Or_Command.
+Expected Results Achieved: Yes
+Concerns: None
+
+Test Number: T8
+Purpose: Ensures the Tokenizer is able to correctly identify a token of kind Literal.
+Expected Results Achieved: Yes
+Concerns: None
+
+Test Number: T9
+Purpose: Ensures the Tokenizer is able to correctly identify a token of kind Comment.
+Expected Results Achieved: Yes
+Concerns: None
+
+Test Number: T10
+Purpose: Ensures the Tokenizer is able to correctly identify a token of kind Number.
+Expected Results Achieved: Yes
+Concerns: None
+
+Test Number: T11
+Purpose: Ensures the Tokenizer is able to correctly identify a token of kind Empty.
+Expected Results Achieved: Yes
+Concerns: None
+
+Test Number: T12
+Purpose: Ensures the Tokenizer is able to correctly identify a token of kind Error.
+Expected Results Achieved: Yes
+Concerns: None
