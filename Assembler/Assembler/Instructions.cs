ad69036c2f732ed9fcdc0f75e66a8b741b3daf8d
Jacob Peddicord <jacob@peddicord.net>
2011-04-08 22:08:46 -0400

    Make Instructions a singleton, and reformat the file.

--- Assembler/Assembler/Instructions.cs
+++ Assembler/Assembler/Instructions.cs
@@ -4,71 +4,79 @@ using System.Collections.Generic;
 
 namespace Assembler
 {
-	public class InstructionException : ApplicationException {}
-	
-	/**
-	 * Opcode translation mechanism. Documentation needs to be written.
-	 */
+    public class InstructionException : ApplicationException
+    {
+        public InstructionException () : base() {}
+        public InstructionException (string s) : base(s) {}
+        public InstructionException (string s, Exception ex) : base(s, ex) {}
+    }
+
+    /**
+     * Contains instructions and their opcodes. Documentation needs to be written.
+     */
     public class Instructions
     {
+        /**
+         * Singleton instance of this class.
+         */
+        private static Instructions instance;
+
+        /**
+         * DOCUMENT ME
+         */
         private Dictionary<string, Dictionary<string, string>> instructions;
-		
-		/**
-		 * Create a translation object. Loads instruction translation data
-		 * from the given file in filename. The file should have lines of
-		 * the format:
-		 *     GROUP FUNCTION BITS
-		 * where GROUP is the instruction group, FUNCTION is the named
-		 * function, and BITS is the corresponding bytecode (as a string of
-		 * 0's and 1's) to the function.
-		 * 
-		 * @param filename file to load data from
-		 */
-        public Instructions (string filename)
+
+        /**
+         * Create a translation object. Loads instruction translation data
+         * from the given file in filename. The file should have lines of
+         * the format:
+         *     GROUP FUNCTION BITS
+         * where GROUP is the instruction group, FUNCTION is the named
+         * function, and BITS is the corresponding bytecode (as a string of
+         * 0's and 1's) to the function.
+         */
+        private Instructions ()
         {
-			this.instructions = new Dictionary<string, Dictionary<string, string>>();
-			
-			/** Responsible for reading the file specified. */
-            TextReader tr = new StreamReader(filename);
-
-			// fill the instruction mapping with data from the file
-			/** Holds the current line in the instruction file. */
-            string line;
-            while ((line = tr.ReadLine()) != null)
-			{
-				/** Contains the current line split by spaces. Always length 3. */
-                string[] parts = line.Split(' ');
-				this.instructions[parts[0]] = new Dictionary<string, string>();
-				this.instructions[parts[0]][parts[1]] = parts[2];
+            this.instructions = new Dictionary<string, Dictionary<string, string>> ();
+            
+            // fill the instruction mapping with data from the file
+            foreach (string line in Properties.Resources.instructions.Split ('\n')) {
+                string[] parts = line.Split (' ');
+                this.instructions[parts[0]] = new Dictionary<string, string> ();
+                this.instructions[parts[0]][parts[1]] = parts[2];
             }
-			
-			// close up
-			tr.Close();
         }
-		
-		/**
-		 * Get a bytecode string corresponding to the proided instruction
-		 * group and function. The returned string is guaranteed to be
-		 * 5 characters (bits) long.
-		 * 
-		 * @param instrGroup Instruction group of function
-		 * @param function Function of group to get bytecode for
-		 * @return string of five '0' and '1' characters
-		 */
-		public string GetBytecodeString (string instrGroup, string function)
-		{
-			if (!this.instructions.ContainsKey(instrGroup))
-			{
-				//throw InstructionException("\"" + instrGroup + "\" is not a valid group.");
-			}
-			
-			if (!this.instructions[instrGroup].ContainsKey(function))
-			{
-				//throw InstructionException("\"" + function + "\" is not a valid function for " + instrGroup);
-			}
-			
-			return this.instructions[instrGroup][function];
-		}
 
+        public static Instructions GetInstance ()
+        {
+            if (Instructions.instance == null) {
+                instance = new Instructions ();
+            }
+            
+            return instance;
+        }
+
+        /**
+         * Get a bytecode string corresponding to the proided instruction
+         * group and function. The returned string is guaranteed to be
+         * 5 characters (bits) long.
+         * 
+         * @param instrGroup Instruction group of function
+         * @param function Function of group to get bytecode for
+         * @return string of five '0' and '1' characters
+         */
+        public string GetBytecodeString (string instrGroup, string function)
+        {
+            if (!this.instructions.ContainsKey (instrGroup)) {
+                throw new InstructionException ("\"" + instrGroup + "\" is not a valid group.");
+            }
+            
+            if (!this.instructions[instrGroup].ContainsKey (function)) {
+                throw new InstructionException ("\"" + function + "\" is not a valid function for " + instrGroup);
+            }
+            
+            return this.instructions[instrGroup][function];
+        }
+        
     }
 }
