eec5c645aa0a1fc6a8afdd76f5c09d44f14c0e19
Mark Mathis <mathis.62@osu.edu>
2011-04-09 21:52:12 -0400

    Added tests for the Directives class.

---
+++ Assembler/Assembler/Tests/DirectivesTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Assembler
+{
+    [TestFixture]
+    public class DirectivesTest
+    {
+        [Test]
+        public void DirectiveExists()
+        {
+            var d = Directives.GetInstance();
+            Assert.IsTrue(d.Contains("START"));
+        }
+
+        [Test]
+        public void DirectiveDoesntExist()
+        {
+            var d = Directives.GetInstance();
+            Assert.IsFalse(d.Contains("derp"));
+        }
+
+        [Test]
+        public void DirectiveCaseChange()
+        {
+            var d = Directives.GetInstance();
+            Assert.IsTrue(d.Contains("EnD"));
+        }
+    }
+}
