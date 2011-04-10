47339bb335c17738b1834ead45a9033da3524366
Jacob Peddicord <jacob@peddicord.net>
2011-04-10 16:09:20 -0400

    Updated SymbolTable's documentation.

--- Assembler/Assembler/SymbolTable.cs
+++ Assembler/Assembler/SymbolTable.cs
@@ -39,23 +39,75 @@ namespace Assembler
         public string val;
     }
 
+    /**
+     * A symbol table. Capable of storing symbols by their rlabel, and keeping
+     * associated information (LC, usage, value) intact. Can output the table
+     * in a sorted fashion.
+     */
     public class SymbolTable
     {
 
+        /**
+         * Representation; holds the label to symbol mapping.
+         */
         private Dictionary<string, Symbol> symbols;
 
+        /**
+         * Create a new symbol table. It is initially empty.
+         *
+         * @refcode
+         * @errtest
+         *  See the test plan for SymbolTable for information.
+         * @errmsg
+         *  None known.
+         * @author Jacob
+         * @creation April 9, 2011
+         * @modlog
+         * @codestandard Mark
+         * @teststandard Andrew
+         */
         public SymbolTable()
         {
             Trace.WriteLine("Creating symbol table", "SymbolTable");   
             this.symbols = new Dictionary<string, Symbol>();
         }
 
+        /**
+         * Add a symbol to the symbol table. Takes a single Symbol object.
+         *
+         * @param symbol Symbol object to add to the table
+         * @refcode
+         * @errtest
+         * @errmsg
+         * @author Jacob
+         * @creation April 9, 2011
+         * @modlog
+         * @codestandard Mark
+         * @teststandard Andrew
+         */
         public void AddSymbol(Symbol symbol)
         {
             Trace.WriteLine(String.Format("Adding {0} to symbol table.", symbol.rlabel), "SymbolTable");
             this.symbols[symbol.rlabel] = symbol;
         }
 
+        /**
+         * Add a symbol to the symbol table. Constructs a Symbol based on the
+         * input parameters, and adds it to the table.
+         *
+         * @param rlabel Label for the constructed symbol
+         * @param lc Location counter for the symbol
+         * @param usage Usage information for the symbol
+         * @param val Optional value for the symbol
+         * @refcode
+         * @errtest
+         * @errmsg
+         * @author Jacob
+         * @creation April 9, 2011
+         * @modlog
+         * @codestandard Mark
+         * @teststandard Andrew
+         */
         public void AddSymbol(string rlabel, string lc, Usage usage, string val)
         {
             Symbol sym;
@@ -66,11 +118,39 @@ namespace Assembler
             this.AddSymbol(sym);
         }
 
+        /**
+         * Get a single symbol by label.
+         *
+         * @param rlabel Label to look up
+         * @return the associated symbol
+         * @refcode
+         * @errtest
+         * @errmsg
+         * @author Jacob
+         * @creation April 9, 2011
+         * @modlog
+         * @codestandard Mark
+         * @teststandard Andrew
+         */
         public Symbol GetSymbol(string rlabel)
         {
             return this.symbols[rlabel];
         }
 
+        /**
+         * Get a list of symbols in the table, sorted by label.
+         *
+         * @return sorted list of symbol labels
+         * @refcode
+         * @errtest
+         *  Tested sorting of symbols
+         * @errmsg
+         * @author Jacob
+         * @creation April 9, 2011
+         * @modlog
+         * @codestandard Mark
+         * @teststandard Andrew
+         */
         public List<string> SortedSymbols()
         {
             var keys = new List<string>();
@@ -82,6 +162,22 @@ namespace Assembler
             return keys;
         }
 
+        /**
+         * Get the symbol table for pretty-printing. Displays, for each symbol:
+         *  - rlabel
+         *  - location counter
+         *  - usage
+         *
+         * @return pretty-printed string of the table
+         * @refcode
+         * @errtest
+         * @errmsg
+         * @author Jacob
+         * @creation April 9, 2011
+         * @modlog
+         * @codestandard Mark
+         * @teststandard Andrew
+         */
         public override string ToString()
         {
             string disp = "---- SYMBOL TABLE ----\n";
