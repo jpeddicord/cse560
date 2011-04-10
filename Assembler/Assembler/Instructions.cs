445d76e11742c858ba0bcb7d661e19603c4405ba
Jacob Peddicord <jacob@peddicord.net>
2011-04-10 15:36:34 -0400

    Finish up draft documentation for Instructions.

--- Assembler/Assembler/Instructions.cs
+++ Assembler/Assembler/Instructions.cs
@@ -4,17 +4,14 @@ using System.Diagnostics;
 
 namespace Assembler
 {
+    /**
+     * Exception thrown when an invalid instruction is looked up.
+     */
     public class InstructionException : ApplicationException
     {
-        public InstructionException() : base()
-        {
-        }
-        public InstructionException(string s) : base(s)
-        {
-        }
-        public InstructionException(string s, Exception ex) : base(s, ex)
-        {
-        }
+        public InstructionException() : base() {}
+        public InstructionException(string s) : base(s) {}
+        public InstructionException(string s, Exception ex) : base(s, ex) {}
     }
 
     /**
@@ -28,7 +25,9 @@ namespace Assembler
         private static Instructions instance;
 
         /**
-         * DOCUMENT ME
+         * Stores the instruction group and function to bitcode map.
+         * The key for the topmost dictionary is the group name, which
+         * contains a map of function to bitcode (as a string).
          */
         private Dictionary<string, Dictionary<string, string>> instructions;
         
@@ -41,12 +40,15 @@ namespace Assembler
          * function, and BITS is the corresponding bytecode (as a string of
          * 0's and 1's) to the function.
          *
-         * @refcode ???
-         * @errtest Things
-         * @errmsg More things
-         * @creation Someday
+         * @refcode
+         * @errtest
+         *  Invalid file formats
+         * @errmsg
+         *  Runtime errors about invalid indexes
+         * @author Jacob
+         * @creation April 7, 2011
          * @modlog
-         *  Jacob, Tuesday, Fixed the herp to the derp
+         *  April 8, 2011 - Jacob - Load from Resource instead of a stream
          * @codestandard Mark
          * @teststandard Andrew
          */
@@ -67,6 +69,19 @@ namespace Assembler
             }
         }
 
+        /**
+         * Get the singleton instance of Instructions.
+         *
+         * @return the Instructions instance
+         * @refcode N/A
+         * @errtest
+         * @errmsg
+         * @author Jacob
+         * @creation April 8, 2011
+         * @modlog
+         * @codestandard Mark
+         * @codestandard Andrew
+         */
         public static Instructions GetInstance()
         {
             Trace.WriteLine("Request of instance of Instructions.", "Instructions");
@@ -78,6 +93,21 @@ namespace Assembler
             return Instructions.instance;
         }
 
+        /**
+         * Check to see if an instruction group and function pair are valid.
+         *
+         * @param instrGroup instruction group to check
+         * @param function function name in group to check
+         * @return whether the function is valid in the given group
+         * @refcode
+         * @errtest
+         * @errmsg
+         * @author Jacob
+         * @creation April 9, 2011
+         * @modlog
+         * @codestandard Mark
+         * @codestandard Andrew
+         */
         public bool IsInstruction(string instrGroup, string function)
         {
             Trace.WriteLine(String.Format("Check if {0} is valid function in {1} category.",
@@ -87,6 +117,20 @@ namespace Assembler
                 && this.instructions[instrGroup.ToUpper()].ContainsKey(function.ToUpper());
         }
 
+        /**
+         * Check to see if an instruction group exists
+         *
+         * @param instrGroup instruction group to check
+         * @return whether the group exists
+         * @refcode
+         * @errtest
+         * @errmsg
+         * @author Jacob
+         * @creation April 9, 2011
+         * @modlog
+         * @codestandard Mark
+         * @codestandard Andrew
+         */
         public bool IsGroup(string instrGroup)
         {
             Trace.WriteLine(String.Format("Check if {0} is valid instruction category", 
@@ -103,6 +147,15 @@ namespace Assembler
          * @param instrGroup Instruction group of function
          * @param function Function of group to get bytecode for
          * @return string of five '0' and '1' characters
+         * @refcode
+         * @errtest
+         * @errmsg
+         * @author Jacob
+         * @creation April 7, 2011
+         * @modlog
+         *  April 8, 2011 - Jacob - throw proper exceptions on invalid group/function pairs
+         * @codestandard Mark
+         * @codestandard Andrew
          */
         public string GetBytecodeString(string instrGroup, string function)
         {
