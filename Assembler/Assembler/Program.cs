aa48020058b044b391d489aa51ce8511677e3176
Mark Mathis <mathis.62@osu.edu>
2011-04-10 23:36:51 -0400

    Added documentation to Program.cs

--- Assembler/Assembler/Program.cs
+++ Assembler/Assembler/Program.cs
@@ -6,14 +6,37 @@ namespace Assembler
 {
     class Program
     {
+        /**
+         * Drives the Assembler.
+         * 
+         * @refcode N/A
+         * @errtest N/A
+         * @errmsg
+         *  - Incorrect number of arguments.
+         *      - Expected 1 parameter, but received [actual args.Length]
+         * @author Mark
+         * @creation April 5, 2011
+         * @modlog
+         *  - April  6, 2011 - Mark - Initializes Logger.
+         *  - April  8, 2011 - Mark - Reads files in from the command line.
+         *  - April  8, 2011 - Mark - Initializes and uses a Parser object.
+         *  - April  8, 2011 - Mark - Outputs results to screen.
+         *  - April  9, 2011 - Mark - Uses an IntermediateFile object.
+         *  - April  9, 2011 - Mark - Catches problem with improper number of arguments.
+         *  - April 10, 2011 - Mark - Uses a SymbolTable object.
+         * @teststandard Andrew Buelow
+         * @codestandard Mark Mathis
+         */
         static void Main(string[] args)
         {
+            // initialize logger
             InitLogger(DateTime.Now);
             Trace.WriteLine("Starting Main method of Assembler", "Main");
 
 
             string[] file = null;
 
+            // check that the arguments are of the proper number
             if (args.Length >= 1)
             {
                 Trace.WriteLine("Arguments are valid", "Main");
@@ -21,6 +44,7 @@ namespace Assembler
             }
             else
             {
+                // bad. exit.
                 string error = "Expected 1 parameter, but received " + args.Length;
                 Trace.WriteLine(error, "Main");
                 Console.Error.WriteLine(error);
@@ -28,6 +52,7 @@ namespace Assembler
                 System.Environment.Exit(1);
             }
 
+            // parse each file passed in through args.  doesn't actually keep parsed output yet.
             foreach (string path in file)
             {
                 Parser pars = new Parser();
@@ -40,6 +65,20 @@ namespace Assembler
             }
         }
 
+        /**
+         * Initializes the Logger, making a Log directory if needed.
+         * 
+         * @refcode N/A
+         * @errtest N/A
+         * @errmsg N/A
+         * @author Mark
+         * @creation April 6, 2011
+         * @modlog
+         *  - April 6, 2011 - Mark - Creates a log file.
+         *  - April 7, 2011 - Mark - Creates Log directory if needed.
+         * @teststandard Andrew Buelow
+         * @codestandard Mark Mathis
+         */
         static void InitLogger(DateTime now)
         {
             // Create the log directory if it doesn't already exist.
