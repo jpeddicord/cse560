00a1d5ca991d76353682b65dea4678eff992b56d
Jacob Peddicord <jacob@peddicord.net>
2011-05-13 21:22:12 -0400

    Document AssemblyReport.

--- Assembler/Assembler/AssemblyReport.cs
+++ Assembler/Assembler/AssemblyReport.cs
@@ -3,21 +3,70 @@ using System.Collections.Generic;
 
 namespace Assembler
 {
+    /**
+     * The assembly report for a single session.
+     */
     public class AssemblyReport
     {
+        /**
+         * A line-item in the report.
+         */
         struct ReportItem {
+            /**
+             * Location of this item
+             */
             public string lc;
+
+            /**
+             * Object code of this line
+             */
             public string objCode;
+
+            /**
+             * A/R/M flag
+             */
             public char flag;
+
+            /**
+             * Source line number
+             */
             public int line;
+
+            /**
+             * Original source line
+             */
             public string source;
+            
+            /**
+             * List of errors associated with this item
+             */
             public List<Errors.Error> errors;
         };
 
+        /**
+         * The list of line-items in the report.
+         */
         private List<ReportItem> items = new List<ReportItem>();
 
         /**
          * Add an item to the assembly report.
+         *
+         * @param lc location of item
+         * @param objCode object code of line
+         * @param flag A/R/M flag
+         * @param line line number in original source
+         * @param source original source line
+         * @param errors list of errors encountered while processing this line
+         * @refcode TODO
+         * @errtest
+         *  N/A
+         * @errmsg
+         *  N/A
+         * @author Jacob Peddicord
+         * @creation May 12, 2011
+         * @modlog
+         * @teststandard Andrew Buelow
+         * @codestandard Mark Mathis
          */
         public void Add(string lc, string objCode, char flag, int line, string source, List<Errors.Error> errors = null)
         {
@@ -33,6 +82,18 @@ namespace Assembler
 
         /**
          * Return the assembly report as a string.
+         *
+         * @return string representation of the report
+         * @refcode TODO
+         * @errtest
+         *  N/A
+         * @errmsg
+         *  N/A
+         * @author Jacob Peddicord
+         * @creation May 12, 2011
+         * @modlog
+         * @teststandard Andrew Buelow
+         * @codestandard Mark Mathis
          */
         public override string ToString()
         {
