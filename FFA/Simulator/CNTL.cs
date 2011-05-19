2af33fd7661af1ad458d0dc38dc6bab3f243e3ea
Andrew Buelow <Andrew@.(none)>
2011-05-19 13:45:21 -0400

    Finished instructions in CNTL

--- FFA/Simulator/CNTL.cs
+++ FFA/Simulator/CNTL.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using ErrCat = Assembler.Errors.Category;
 
@@ -15,12 +14,14 @@ namespace Simulator
 
         public static void Halt(int code)
         {
+            // TODO Throw error if out of range
             Console.WriteLine("Program exited with code: " + code);
             System.Environment.Exit(code);
         }
 
         public static void Dump(short flag)
         {
+            // User should only provide a 1, 2 or 3 as a dump flag
             switch (flag)
             {
                 case 1:
@@ -72,11 +73,15 @@ namespace Simulator
 
         private static void dumpStack()
         {
+            // Get both stacks as Arrays
             string[] data = Memory.GetInstance().GetDataStack();
             short[] test = Memory.GetInstance().GetTestStack();
 
+            // Print the "header" for data stack
             Console.Write("Data Stack: ");
 
+            // Only attempt to print the first item in the data stack if it exists
+            // Otherwise, pad with spaces to make the test stack line up correctly
             if (data.Length > 0)
             {
                 Console.Write(data[0]);
@@ -86,44 +91,63 @@ namespace Simulator
                 Console.Write("    ");
             }
 
+            // Print "header" for test stack
             Console.Write("  Test Stack: ");
 
+            // Only attempt to print the first item in the test stack if it exists
             if (test.Length > 0)
             {
                 Console.Write(test[0]);
-                Console.Write('\n');
             }
 
-            int n = 1;
+            Console.Write('\n');
 
-            while (n < data.Length || n < test.Length)
+            // Loop through until the end of both arrays has been reached
+            for (int n = 1; n < data.Length || n < test.Length; n++)
             {
+                // Print padding to line up data stack output
                 Console.Write("            ");
 
+                // If there is another item in data stack print it, otherwise
+                // pad with spaces
                 if (n < data.Length)
                 {
                     Console.Write(data[n]);
-                    Console.Write("              ");
                 }
                 else
                 {
-                    Console.Write("                  ");
+                    Console.Write("    ");
                 }
 
+                // If an item exists in the test stack, pad to line up test stack output
+                // convert to hex and pad with 0s, then output.
                 if (n < test.Length)
                 {
+                    Console.Write("              ");
                     Console.Write(Convert.ToString(test[n], 16).PadLeft(4, '0'));
                 }
 
                 Console.Write('\n');
-
-                n++;
             }
         }
 
         private static void dumpMem()
         {
-            
+            // Handles each row of memory
+            for (int i = 0; i <= 1023; i = i + 16)
+            {
+                // Print the memory offset on this row
+                Console.Write(Convert.ToString(i, 16).PadLeft(3, '0'));
+
+                // Prints each item in the row
+                for (int j = 0; j < 16; j++)
+                {
+                    // Print the memory at memory offset + position in this row.
+                    Console.Write(" " + Memory.GetInstance().GetWord(i + j));
+                }
+
+                Console.Write("\n");
+            }
         }
     }
 }
