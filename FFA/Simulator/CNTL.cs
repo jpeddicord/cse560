c9483358b44e0e9bd23d4dcd5027c614a91e1844
Andrew Buelow <Andrew@.(none)>
2011-05-19 00:35:41 -0400

    Added resource and CNTL

---
+++ FFA/Simulator/CNTL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simulator
+{
+    class CNTL
+    {
+        public CNTL()
+        {
+            // Possibly logging?
+        }
+
+        public static void Halt(int code)
+        {
+
+        }
+    }
+}
