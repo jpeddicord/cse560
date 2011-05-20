a664f5dcd202a3932ab2da732e8ce84321bfe344
Andrew Buelow <Andrew@.(none)>
2011-05-19 21:31:20 -0400

    Added SOPER operations

--- FFA/Simulator/SOPER.cs
+++ FFA/Simulator/SOPER.cs
@@ -95,5 +95,88 @@ namespace Simulator
                 }
             }
         }
+
+        public static void Div(int n)
+        {
+            if (n > 1)
+            {
+                try
+                {
+                    Memory m = Memory.GetInstance();
+                    int total = Convert.ToInt32(m.DataPop(), 16);
+
+                    while (n > 1)
+                    {
+                        int i = Convert.ToInt32(m.DataPop(), 16);
+
+                        if (i == 0)
+                        {
+                            throw new Assembler.ErrorException(ErrCat.Serious, 12);
+                        }
+
+                        total = total / Convert.ToInt32(m.DataPop(), 16);
+                    }
+
+                    if (total > 65535)
+                    {
+                        throw new Assembler.ErrorException(ErrCat.Serious, 11);
+                    }
+
+                    m.DataPush(Convert.ToString(total, 16));
+                }
+                catch (Exception)
+                {
+                    // TODO Handle error nothing on data stack
+                }
+            }
+        }
+
+        public static void Or(int n)
+        {
+            if (n > 1)
+            {
+                try
+                {
+                    Memory m = Memory.GetInstance();
+                    int total = Convert.ToInt32(m.DataPop(), 16);
+
+                    while (n > 1)
+                    {
+                        total = total | Convert.ToInt32(m.DataPop(), 16);
+                    }
+
+                    m.DataPush(Convert.ToString(total, 16));
+
+                }
+                catch (Exception)
+                {
+                    // TODO Handle error nothing on data stack
+                }
+            }
+        }
+
+        public static void And(int n)
+        {
+            if (n > 1)
+            {
+                try
+                {
+                    Memory m = Memory.GetInstance();
+                    int total = Convert.ToInt32(m.DataPop(), 16);
+
+                    while (n > 1)
+                    {
+                        total = total & Convert.ToInt32(m.DataPop(), 16);
+                    }
+
+                    m.DataPush(Convert.ToString(total, 16));
+
+                }
+                catch (Exception)
+                {
+                    // TODO Handle error nothing on data stack
+                }
+            }
+        }
     }
 }
