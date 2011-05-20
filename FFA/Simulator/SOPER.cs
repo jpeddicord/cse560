05a050fd285c66391a15923f2d301fbadc8955c7
Andrew Buelow <Andrew@.(none)>
2011-05-19 20:27:10 -0400

    Finished operations for CNTL, added a few of the operations for SOPER. Error catching needs to be done for both still.

---
+++ FFA/Simulator/SOPER.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ErrCat = Assembler.Errors.Category; 
+
+namespace Simulator
+{
+    class SOPER
+    {
+        public SOPER()
+        {
+            // Logger?
+        }
+
+        public static void Add(int n)
+        {
+            if (n > 1)
+            {
+                try
+                {
+                    Memory m = Memory.GetInstance();
+                    int total = 0;
+                    while (n > 0)
+                    {
+                        total = total + Convert.ToInt32(m.DataPop(), 16);
+                    }
+
+                    if (total > 65535)
+                    {
+                        throw new Assembler.ErrorException(ErrCat.Serious, 11);
+                    }
+
+                    m.DataPush(Convert.ToString(total, 16));
+                }
+                catch (Exception)
+                {
+                    // TODO Handle error nothing on data stack
+                }
+            }
+        }
+
+        public static void Sub(int n)
+        {
+            if (n > 1)
+            {
+                try
+                {
+                    Memory m = Memory.GetInstance();
+                    int total = Convert.ToInt32(m.DataPop(), 16);
+
+                    while (n > 1)
+                    {
+                        total = total - Convert.ToInt32(m.DataPop(), 16);
+                    }
+
+                    if (total < 0)
+                    {
+                        throw new Assembler.ErrorException(ErrCat.Serious, 11);
+                    }
+
+                    m.DataPush(Convert.ToString(total, 16));
+
+                }
+                catch (Exception)
+                {
+                    // TODO Handle error nothing on data stack
+                }
+            }
+        }
+
+        public static void Mul(int n)
+        {
+            if (n > 1)
+            {
+                try
+                {
+                    Memory m = Memory.GetInstance();
+                    int total = 1;
+                    while (n > 0)
+                    {
+                        total = total * Convert.ToInt32(m.DataPop(), 16);
+                    }
+
+                    if (total > 65535)
+                    {
+                        throw new Assembler.ErrorException(ErrCat.Serious, 11);
+                    }
+
+                    m.DataPush(Convert.ToString(total, 16));
+                }
+                catch (Exception)
+                {
+                    // TODO Handle error nothing on data stack
+                }
+            }
+        }
+    }
+}
